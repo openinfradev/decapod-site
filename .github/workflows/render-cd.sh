35bfc17092a10dfe6d78db5fdb2714c34cb3ad45
Taekyu <ktkfree@naver.com>
2022-11-29 20:32:51 +0900

    trivial. use ft branch

--- .github/workflows/render-cd.sh
+++ .github/workflows/render-cd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DECAPOD_BASE_URL=https://github.com/openinfradev/decapod-base-yaml.git
-BRANCH="main"
+BRANCH="ft"
 DOCKER_IMAGE_REPO="docker.io"
 GITHUB_IMAGE_REPO="ghcr.io"
 outputdir="output"
